96d98c079db46c633a8fa8bc736ced08a10eaa7f
Lauritz Holtmann <security@lauritz-holtmann.de>
2024-11-12 14:02:32 +0100

    feat(): Add option to exclude domains from scans

--- options.js
+++ options.js
@@ -6,9 +6,10 @@ const WEEK_IN_MS = 7 * 24 * 60 * 60 * 1000;
 
 const saveOptions = () => {
     const cacheDuration = document.getElementById('cache-duration').value;
+    const ignoredDomains = document.getElementById('ignored-domains').value.split('\n').map(domain => domain.trim()).filter(domain => domain);
 
     chrome.storage.sync.set(
-        { cacheDuration },
+        { cacheDuration, ignoredDomains },
         () => {
             // Update status to let user know options were saved.
             const status = document.getElementById('status');
@@ -22,9 +23,10 @@ const saveOptions = () => {
 
 const restoreOptions = () => {
     chrome.storage.sync.get(
-        { cacheDuration: WEEK_IN_MS },
+        { cacheDuration: WEEK_IN_MS, ignoredDomains: [] },
         (items) => {
             document.getElementById('cache-duration').value = items.cacheDuration;
+            document.getElementById('ignored-domains').value = items.ignoredDomains.join('\n');
         }
     );
 };
