62a70848eb001525480876f579961ddf71d92ce1
Lauritz Holtmann <security@lauritz-holtmann.de>
2024-11-18 13:15:51 +0100

    Add build script for Chrome Store

---
+++ build_extension.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Get the current date in YYYYMMDD format
+CURRENT_DATE=$(date +"%Y%m%d")
+
+# Name of the zip file with the current date
+ZIP_FILE="dead_domain_discovery_$CURRENT_DATE.zip"
+
+# List of files to include in the zip file based on the manifest
+FILES=(
+  "manifest.json"
+  "background.js"
+  "popup.html"
+  "options.html"
+  "icons/icon16.png"
+  "icons/icon32.png"
+  "icons/icon48.png"
+  "icons/icon128.png"
+)
+
+# Remove the zip file if it already exists
+if [ -f "$ZIP_FILE" ]; then
+  rm "$ZIP_FILE"
+fi
+
+# Create a new zip file with the specified files
+zip "$ZIP_FILE" "${FILES[@]}"
+
+echo "Extension has been bundled into $ZIP_FILE"
